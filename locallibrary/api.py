68ce828fb701e842a3e8f1ce3faa1e2fb085a38a
igor-golov <oldghost07@mail.ru>
2025-01-24 16:58:36 +0300

    Add NinjaAPI

--- locallibrary/api.py
+++ locallibrary/api.py
@@ -1,9 +1,22 @@
 from ninja import NinjaAPI
+from typing import Union
 api=NinjaAPI()
 
 @api.get("/")
 async def index(request):
   return {"message":"Hello World"}
+
 @api.get("/hello")
 async def Hello(request,name="World"):
   return {"message":f"Hello {name}"}
+
+@api.get("/math")
+async def math(request, a:Union[int,None]=None,b:Union[int, None]=None):
+  if not(a is None) and not(b is None):
+    return {"add":a+b,"multiply":a*b,"divide":a/b,"subtract":a-b}
+  else:
+    return {"message":"Please enter two numbers"}
+
+@api.get("/items/{item_id}")
+async def items(request,item_id):
+  return {"item_id":item_id}
