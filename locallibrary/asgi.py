19b77da3698107490e0429e070191059e5e392ee
igor-golov <oldghost07@mail.ru>
2025-01-24 07:44:56 +0300

    Add FastAPI

--- locallibrary/asgi.py
+++ locallibrary/asgi.py
@@ -7,10 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'locallibrary.settings')
+
+# application = get_asgi_application()
+
+# myproject/asgi.py
+
 import os
-
 from django.core.asgi import get_asgi_application
+from fastapi.middleware.wsgi import WSGIMiddleware
+# from fastapi_app import app as fastapi_app
+import fastapi_app
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'locallibrary.settings')
-
-application = get_asgi_application()
+django_app = get_asgi_application()
+app = WSGIMiddleware(django_app)
+app.mount("/api", fastapi_app)
